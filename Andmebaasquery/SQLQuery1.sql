a59c2c4b93db8c73ffb4796c0e0eab05da36d6b1
RasmusKopli <kopli.rasmus@gmail.com>
2026-02-17 14:09:50 +0200

    Create SQLQuery1.sql

---
+++ Andmebaasquery/SQLQuery1.sql
@@ -0,0 +1,52 @@
+-- teeme andmebaasi ehk db
+create database IKT25tar
+
+--andmebaasi valimine
+use IKT25tar
+
+--andmebaasi kustutamine koodiga
+drop database IKT25tar
+
+--teeme uuesti andmebaasi IKT25tar
+create database IKT25tar
+
+--teeme tabeli
+create table Gender
+(
+--Meil on muutuja Id,
+--mis on täisarv andmetüüp,
+--kui sisestad andmed,
+--siis see veerg peab olema täidetud
+--tegemist on primaarvõtmega
+Id int not null primary key,
+--veeru nimi on Gender,
+--10 tähemärki on max pikkus,
+--andmed peavad olema sisestatud ehk
+--ei tohi olla tühi
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine Gender tabelisse
+--Id = 1, Gender = Male
+--Id = 2, Gender = Female
+insert into Gender (Id, Gender)
+values (1, 'Male'),
+(2, 'Female')
+
+--vaatame tabeli sisu
+-- * tähendab, et näita kõike seal sees olevat infot
+select * from Gender
+
+--teeme tabeli nimega Person
+--veeru nimed: Id int not null primary key,
+--Name nvarchar (30)
+--Email nvarchar (30)
+--Genderid int
+
+Create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int
+)
